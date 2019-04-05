60dc79d3ff25780c5195b4f1c8f017eebf85e44a
Simon Benvenutto <simon@benvenutto.com>
2019-04-05 11:31:55 +0100

    try stdout redirect to log again

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-	nvidia-docker run -d --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer jupyter >/tmp/jupyter.log
+	nvidia-docker run -d --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer jupyter
+	
