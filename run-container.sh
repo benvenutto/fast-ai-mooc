0176578cb45c3d1c09f0dd587daeeac2230d8516
Simon Benvenutto <simon@benvenutto.com>
2019-04-07 16:14:47 +0100

    update new image name

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-	nvidia-docker run -it --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer jupyter
+	nvidia-docker run -it --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.03-py3"
 
