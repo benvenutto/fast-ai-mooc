78f16bcfbe5ec3c4015b456866d55595e6ab4521
Simon Benvenutto <simon@benvenutto.com>
2019-07-06 20:43:48 +0100

    upgrade to pytorch:19.06-py3 container

--- run-container.sh
+++ run-container.sh
@@ -3,5 +3,5 @@
 	sudo nvidia-smi -pm ENABLED -i 0 && \
     sudo nvidia-smi -pl 217 -i 0
 
-	nvidia-docker run -it --name jupyter --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.03-py3"${1}
+	nvidia-docker run -it --name jupyter --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.06-py3"${1}
 
