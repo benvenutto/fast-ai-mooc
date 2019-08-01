bb67b495de27803fe42a142a0cfecb89df2446ea
Simon Benvenutto <simon@benvenutto.com>
2019-08-01 14:45:52 +0100

    delete image on completion

--- run-container.sh
+++ run-container.sh
@@ -3,5 +3,5 @@
 	sudo nvidia-smi -pm ENABLED -i 0 && \
     sudo nvidia-smi -pl 217 -i 0
 
-	nvidia-docker run --rm -it --name jupyter --ipc=host -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.07-py3"${1}
+	nvidia-docker run -it --name jupyter --ipc=host --rm -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.07-py3"${1}
 
