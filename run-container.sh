9c288251fc35a9b7485143a19c9bfd4b2296b7b0
Simon Benvenutto <simon@benvenutto.com>
2019-09-21 14:17:39 +0100

    update to nvcr.io/nvidia/pytorch:19.09-py3

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-	nvidia-docker run --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.07-py3"${1}
+	nvidia-docker run --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.09-py3"${1}
 
