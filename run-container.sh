2988f61d1c740fde7d85765ac6b69f64898f239a
simonb <fr4nc3sc4>
2020-01-12 10:28:05 +0000

    pytorch:19.12

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-	docker run --gpus all --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.10-py3"${1}
+	docker run --gpus all --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:19.12-py3"${1}
 
