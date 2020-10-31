c75433cd1c1672d10f84cc3b469d660b5931eebc
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-10-31 10:13:02 +0000

    migrate to pytorch:20.09-py3 (too many performance regressions in pytorch:20.10-py3)

--- run-container.sh
+++ run-container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-	docker run --gpus all --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.07-py3"${1}
+	docker run --gpus all --cap-add=SYS_ADMIN -it --name jupyter --ipc=host --restart unless-stopped -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.09-py3"${1}
