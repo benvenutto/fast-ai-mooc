04d1843f27ff97126c445800c7ce1a48cb14816a
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-11-08 18:14:37 +0000

    add Kaggle CLI and Albumenations augmentation library

--- run-udacity.sh
+++ run-udacity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-	docker run --gpus all --cap-add=SYS_ADMIN -it --name udacity --ipc=host -p 8080:8080 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.09-py3.udacity"
+	docker run --gpus all --cap-add=SYS_ADMIN -it --name udacity --ipc=host -p 8888:8888 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.09-py3.udacity"
