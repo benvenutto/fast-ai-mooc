c51217dc0e8b3322dc9b2e810a52add618759678
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-11-30 04:14:17 +0000

    add localhost postgres port (5432)

--- run-udacity.sh
+++ run-udacity.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-	docker run --gpus all --cap-add=SYS_ADMIN -it --name udacity --rm --ipc=host -p 8888:8888 -p localhost:5432:5432 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.10-py3.udacity"
+	docker run --gpus all --cap-add=SYS_ADMIN -it --name udacity --rm --ipc=host -p 8888:8888 -p 127.0.0.1:5432:5432 -v /home/simonb/Developer:/Docker/Developer "nvidia/pytorch/fastai:20.10-py3.udacity"
