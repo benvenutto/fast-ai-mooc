5c67c2d23ff776623e6319413cfb70be46aac32a
Simon Benvenutto <simon.benvenutto@gmail.com>
2020-12-25 23:06:47 +0000

    remove jupyter fixed login token as no longer using with PyCharm

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,2 +1,2 @@
 c.NotebookApp.password = u'sha1:dda712d8e6f1:b73d81002a7900a978b770b82300b433f09d875e'
-c.NotebookApp.token = '362F3522-D0A4-428F-8D56-E2ED4B43D1BC'
+# c.NotebookApp.token = '362F3522-D0A4-428F-8D56-E2ED4B43D1BC'
